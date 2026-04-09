8fd81f52d49f5c855bb042ab00bbe803904ba811
yashpatle741 <yashpatle99@gmail.com>
2026-04-09 13:52:43 +0530

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("DevOps Project Running 🚀");
+});
+
+// Health check route (VERY IMPORTANT in real projects)
+app.get("/health", (req, res) => {
+  res.send("App is healthy ✅");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
